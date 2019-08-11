d9b91b56ac8b7d21da90daf1db326caa62a8abf1
Coxhuang <mhesat@outlook.com>
2019-08-11 10:59:06 +0800

    first
    first

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div class="tab">
+			<div class="tab-item">
+				<router-link to="/goods">商品</router-link>
+			</div>
+			<div class="tab-item">
+				<router-link to="/ratings">评价</router-link>
+			</div>
+			<div class="tab-item">
+				<router-link to="/seller">商家</router-link>
+			</div>
+		</div>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.tab
+		display:flex
+		width : 100%
+		height : 40px
+		line-height 40px
+		.tab-item
+			flex :1
+			text-align :center
+</style>
